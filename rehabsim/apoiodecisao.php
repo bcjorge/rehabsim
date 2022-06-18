77b858f034593722bb8d7cf8e797750485e4eb2e
cvjorge2000 <104597250+cvjorge2000@users.noreply.github.com>
2022-06-18 22:08:02 +0100

    Update apoiodecisao

--- rehabsim/apoiodecisao.php
+++ rehabsim/apoiodecisao.php
@@ -1792,3 +1792,140 @@ if (isset($_SESSION["authuser"])&&$_SESSION["authuser"]==1) {
     }
 
 ?>
+<!DOCTYPE html>
+<html lang=”en”>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap" rel="stylesheet">
+    <link rel="icon" type="image/x-icon" href="assets/images/RehabSIM_logo_colr.png">
+
+    <title>RehabSIM</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!--
+
+    TemplateMo 570 Chain App Dev
+
+    https://templatemo.com/tm-570-chain-app-dev
+
+    -->
+
+    <!-- Additional CSS Files -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/css/templatemo-chain-app-dev.css">
+    <link rel="stylesheet" href="assets/css/pag.css">
+    <link rel="stylesheet" href="assets/css/animated.css">
+    <link rel="stylesheet" href="assets/css/owl.css">
+</head>
+<body>
+
+<!-- ***** Header Area Start ***** -->
+<header class="header-area header-sticky wow slideInDown" data-wow-duration="0.75s" data-wow-delay="0s">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <nav class="main-nav">
+                    <!-- ***** Logo Start ***** -->
+                    <a href="index.php" class="logo">
+                        <img src="assets/images/logo_name_small.png" alt="RehabSIM logo">
+                    </a>
+                    <!-- ***** Logo End ***** -->
+                    <!-- ***** Menu Start ***** -->
+                    <ul class="nav">
+                        <li class="scroll-to-section"><a href="index.php">Home</a></li>
+                        <li class="scroll-to-section"><a href="index.php">About</a></li>
+                        <li><div class="gradient-button"><a href="login.php?action=logout"><i class="fa fa-sign-in-alt"></i> Logout</a></div></li>
+                    </ul>
+                    <a class='menu-trigger'>
+                        <span>Menu</span>
+                    </a>
+                    <!-- ***** Menu End ***** -->
+                </nav>
+            </div>
+        </div>
+    </div>
+</header>
+<!-- ***** Header Area End ***** -->
+
+<div class="containerInfo">
+    <div class="text">
+        <h4>Terapeuta</h4>
+        <h1><?php //echo $data['nome']?></h1>
+    </div>
+    <div class="containerBlocks">
+        <div class="profile">
+            <form class="login" method="POST" action="pag_terapeuta.php?action=pag_terapeuta" enctype="multipart/form-data">
+                <h4>Informação do Utilizador</h4>
+                <div class="row">
+                    <div class="column c1">
+
+                    </div>
+                    <div class="column c2">
+
+                    </div>
+                    <div class="column c3">
+
+                        <input class="savButton" type="submit"  name="update" value="Salvar Alterações">
+                    </div>
+                </div>
+            </form>
+        </div>
+        <div class="pac">
+            <h4>Registo de Paciente</h4>
+            <div class="formPaciente">
+
+                    <div class="column c1">
+
+                    </div>
+                    <div class="column c2">
+
+                    </div>
+                    <div class="column c3">
+
+                        <input class="gradient-button sessionButton2"type="submit"  name="registar_p" value="Registar Paciente"</input>
+                    </div>
+            </div>
+        </div>
+        <div class="pl">
+            <div class="column c1">
+                <h5> Procurar Paciente existente</h5>
+                <div class="formPaciente">
+
+                </div>
+                <br>
+                <br>
+                <br>
+                <h5> Histórico de Consultas</h5>
+
+
+            </div>
+
+            <div class="column c2">
+                <h5> Inserir Nova Consulta </h5>
+                <div class="formPaciente">
+                </div>
+            </div>
+            <div class="column c3">
+                <div class="formPaciente">
+
+                    <input class="gradient-button sessionButton1" type="submit" value="Iniciar Consulta" name="gravar_dados">
+                </div>
+            </div>
+
+
+            <!-- Scripts -->
+            <script src="vendor/jquery/jquery.min.js"></script>
+            <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+            <script src="assets/js/owl-carousel.js"></script>
+            <script src="assets/js/animation.js"></script>
+            <script src="assets/js/imagesloaded.js"></script>
+            <script src="assets/js/custom.js"></script>
+</body>
+</html>
